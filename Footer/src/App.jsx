b599bed55a9d90f93148e537d74de304d3ce1c51
Yasitha Sanjula Herath <yasithasanjula2018@gmail.com>
2023-11-13 23:30:30 +0530

    111

---
+++ Footer/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import Footer from './Footer'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
